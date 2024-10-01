ab4945daef868186e749bd6ba01f28891ae4962a
Afira <afiratahmali@gmail.com>
2024-10-01 22:31:16 +0800

    added project delete view

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -63,6 +63,11 @@ class ProjectDetail(APIView):
             serializer.errors,
             status=status.HTTP_400_BAD_REQUEST
         )
+    
+    def delete(self, request, pk):
+        project = self.get_object(pk)
+        project.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class PledgeList(APIView):
