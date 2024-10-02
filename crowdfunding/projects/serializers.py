59b82ad8c02a50b3f74925b993ffc1d5edc3b677
Afira <afiratahmali@gmail.com>
2024-10-02 21:34:55 +0800

    added a PledgeDetail view, serializer and URL path

--- crowdfunding/projects/serializers.py
+++ crowdfunding/projects/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.apps import apps
 
+
 class PledgeSerializer(serializers.ModelSerializer):
     supporter = serializers.ReadOnlyField(source='supporter.id')
 
@@ -8,12 +9,26 @@ class PledgeSerializer(serializers.ModelSerializer):
         model = apps.get_model('projects.Pledge')
         fields = '__all__'
 
+
 class ProjectSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.id')
 
     class Meta:
         model = apps.get_model('projects.Project')
         fields = '__all__'
+
+
+class PledgeDetailSerializer(PledgeSerializer):
+    project = ProjectSerializer(read_only=True)
+
+    def update(self, instance, validated_data):
+        instance.amount = validated_data.get('amount', instance.amount)
+        instance.comment = validated_data.get('comment', instance.comment)
+        instance.anonymous = validated_data.get('anonymous', instance.anonymous)
+        instance.supporter = validated_data.get('supporter', instance.supporter)
+        instance.save()
+        return instance
+
 
 class ProjectDetailSerializer(ProjectSerializer):
     pledges = PledgeSerializer(many=True, read_only=True)
