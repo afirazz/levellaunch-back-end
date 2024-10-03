4a874426f831c29b0ec287cdae0c4c31caffd995
Afira <afiratahmali@gmail.com>
2024-10-03 23:43:01 +0800

    added IsUserOrReadOnly permission to CustomUserDetail view

--- crowdfunding/users/views.py
+++ crowdfunding/users/views.py
@@ -1,11 +1,12 @@
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from .models import CustomUser
 from .serializers import CustomUserSerializer
+from .permissions import IsUserOrReadOnly
 
 
 class CustomUserList(APIView):
@@ -29,6 +30,11 @@ class CustomUserList(APIView):
 
 
 class CustomUserDetail(APIView):
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly,
+        IsUserOrReadOnly
+    ]
+
     def get_object(self, pk):
         try:
             return CustomUser.objects.get(pk=pk)
@@ -40,6 +46,23 @@ class CustomUserDetail(APIView):
         serializer = CustomUserSerializer(user)
         return Response(serializer.data)
     
+    def put(self, request, pk):
+        user = self.get_object(pk)
+        self.check_object_permissions(request, user)
+        serializer = CustomUserSerializer(
+            instance=user,
+            data=request.data,
+            partial=True
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+ 
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
 
 class CustomAuthToken(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
