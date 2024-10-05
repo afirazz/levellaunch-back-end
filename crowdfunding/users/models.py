f659ad29a41b9ad85c022d39cad02020b1299da8
Afira <afiratahmali@gmail.com>
2024-10-05 17:26:34 +0800

    added image and bio fields to CustomUser model

--- crowdfunding/users/models.py
+++ crowdfunding/users/models.py
@@ -1,6 +1,10 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class CustomUser(AbstractUser):
+    image = models.URLField(default="https://www.shutterstock.com/image-vector/blank-avatar-photo-place-holder-600nw-1095249842.jpg")
+    bio = models.TextField(blank=True)
 
     def __str__(self):
         return self.username
