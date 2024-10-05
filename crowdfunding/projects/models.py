5a6e8f39d838178f4d31a97a22e6644922bb5e85
Afira <afiratahmali@gmail.com>
2024-10-05 17:14:41 +0800

    added additional fields to Project model

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -3,12 +3,22 @@ from django.contrib.auth import get_user_model
 
 
 class Project(models.Model):
+    GAME_TYPE_CHOICES = {
+        "video": "Video Game",
+        "tabletop": "Tabletop Game",
+        "mobile": "Mobile Game",
+        "cards": "Playing Cards",
+    }
+
     title = models.CharField(max_length=200)
     description = models.TextField()
+    game_type = models.CharField(max_length=20, choices=GAME_TYPE_CHOICES)
     goal = models.IntegerField()
     image = models.URLField()
     is_open = models.BooleanField()
     date_created = models.DateTimeField(auto_now_add=True)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
     owner = models.ForeignKey(
         get_user_model(),
         on_delete=models.CASCADE,
